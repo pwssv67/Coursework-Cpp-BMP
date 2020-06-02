d9a1015d538f87e2b9b74f77e4375be58ab133be
pwssv67 <61920177+pwssv67@users.noreply.github.com>
2020-06-03 01:18:21 +0300

    Добавьте файлы проекта.

---
+++ TP Coursework/TP Coursework.cpp
@@ -0,0 +1,38 @@
+﻿#include <minwindef.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+struct BITMAPFILEHEADER {
+	WORD    Type;     // ‘BM’ 0x4D42   
+	DWORD   Size;     // Размер файла в байтах, BitCount*Height*Width+ OffsetBits   
+	WORD    Reserved1;    // Зарезервирован; должен быть нуль  
+	WORD    Reserved2;    // Зарезервирован; должен быть нуль  
+	DWORD   OffsetBits;   // Смещение данных от начала файла в байтах // = sizeof(BITMAPFILEHEADER)+sizeof(BITMAPINFOHEADER) 
+}; 
+
+struct BITMAPINFOHEADER {
+	DWORD   Size;           // Число байтов необходимое для структуры = 40  
+	DWORD   Width;          // Ширина точечного рисунка в пикселях   
+	DWORD   Height;         // Высота точечного рисунка в пикселях   
+	WORD    Planes;         // Число плоскостей целевого устройства = 1 
+	WORD    BitCount;       // Глубина цвета, число бит на точку = 0,1,4,8,16,24,32   
+	DWORD   Compression;    // Тип сжатия = 0 для несжатого изображения   
+	DWORD   SizeImage;      // Размер изображения в байтах BitCount*Height*Width  
+	DWORD   XPelsPerMeter;  // Разрешающая способность по горизонтали  
+	DWORD   YPelsPerMeter;  // Разрешающая способность по вертикали  
+	DWORD   ColorUsed;      // Число индексов используемых цветов. Если все цвета = 0  
+	DWORD   ColorImportant; // Число необходимых цветов = 0
+};
+
+
+struct RGBQUAD {
+	BYTE Blue;
+	BYTE Green;
+	BYTE Red;
+	BYTE Reserved;
+};
+
+class Image {
+	RGBQUAD **Rgbquad;
+	BITMAPINFOHEADER BMInfoHeader;
+
+};
